79adbeb05cb15eaa7eaa8a6cf1cef24973a483c1
Jonny Kalambay <jonnyk20@gmail.com>
2017-09-11 18:25:31 +0000

    url concat working

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,18 @@
 var request = require('request');
 
-console.log('Welcome to the GitHub Avatar Downloader!');
+function getRepoContributors(repoOwner, repoName, cb) {
+  // ...
+}
+
+var repoOwner = "jquery";
+var repoName = "jquery";
+var GITHUB_USER = "jonnyk20";
+var GITHUB_TOKEN = "9bb52bcea1faeb39411c7e009a819febaadc9371";
+var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
+
+console.log(requestURL);
