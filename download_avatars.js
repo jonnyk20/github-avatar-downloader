488ca1e33ff1d5c64bac6f66658a28cb09e8421a
Jonny Kalambay <jonnyk20@gmail.com>
2017-09-11 21:36:36 +0000

    removing promises

--- download_avatars.js
+++ download_avatars.js
@@ -7,26 +7,31 @@ const GITHUB_USER = process.env.DB_USER;
 const GITHUB_TOKEN = process.env.DB_PASS;
 
 function getRepoContributors(repoOwner, repoName, cb) {
-var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+  var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
   if (!repoOwner || !repoName) {
     console.log("Please enter repo name and owner");
     return;
   }
 
   var contributors = [];
-  request.get(requestURL, options,function(error, response, body){
-    Promise.resolve( // <- Is this necessary?
-    contributors = JSON.parse(body)).then(function(){
-      contributors.forEach(function(person) {
-        // console.log("Login:",person.login);
-        // console.log("Avatar:",person.avatar_url);
-        downloadImageByURL(person.avatar_url, "./avatars/" + person.login + ".jpg" );
-      });
-    })
+  request.get(requestURL, options, function(error, response, body){
+    contributors = JSON.parse(body);
+    contributors.forEach(function(person) {
+      console.log("Login:", person.login);
+      console.log("Avatar:", person.avatar_url);
+    });
+    // Promise.resolve( // <- Is this necessary?
+    // contributors = JSON.parse(body)).then(function(){
+    //   contributors.forEach(function(person) {
+    //     // console.log("Login:",person.login);
+    //     // console.log("Avatar:",person.avatar_url);
+    //     downloadImageByURL(person.avatar_url, "./avatars/" + person.login + ".jpg" );
+    //   });
+    // })
   })               
   .on('error', function (err) {    
-    console.log(err);                             
-    throw err; 
+    console.log("yo momma", err);                             
+    throw err;
   })      
 }
 
@@ -35,14 +40,11 @@ function downloadImageByURL(url, filePath) {
   request.get(url, options,function(error, response, body){
   })
   .pipe(fs.createWriteStream(filePath))
-  .on('error', function (err) {    
-    console.log(err);                             
-    throw err; 
-  });            
+  .on('error', function (err) {
+    console.log(err);
+    throw err;
+  });
 }
-
-
-
 
 
 var options = {
