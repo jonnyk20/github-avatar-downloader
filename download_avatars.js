47983a19c4f1ec53269475ff3c9c663ad7771182
Jonny Kalambay <jonnyk20@gmail.com>
2017-09-11 22:23:54 +0000

    cleaned up code

--- download_avatars.js
+++ download_avatars.js
@@ -1,10 +1,15 @@
 var request = require('request');
 var fs = require('fs');
 var dotenv = require('dotenv');
-dotenv.config({path: './credentials.env'});
+dotenv.config({path: './.env'});
 
 const GITHUB_USER = process.env.DB_USER;
 const GITHUB_TOKEN = process.env.DB_PASS;
+var options = {
+  headers: {
+    'User-Agent': "GitHub Avatar Downloader - Student Project"
+  }
+};
 
 function getRepoContributors(repoOwner, repoName, cb) {
   var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
@@ -12,50 +17,38 @@ function getRepoContributors(repoOwner, repoName, cb) {
     console.log("Please enter repo name and owner");
     return;
   }
-
   var contributors = [];
-  request.get(requestURL, options, function(error, response, body){
-    contributors = JSON.parse(body);
-    contributors.forEach(function(person) {
-      console.log("Login:", person.login);
-      console.log("Avatar:", person.avatar_url);
-    });
-    // Promise.resolve( // <- Is this necessary?
-    // contributors = JSON.parse(body)).then(function(){
-    //   contributors.forEach(function(person) {
-    //     // console.log("Login:",person.login);
-    //     // console.log("Avatar:",person.avatar_url);
-    //     downloadImageByURL(person.avatar_url, "./avatars/" + person.login + ".jpg" );
-    //   });
-    // })
-  })               
-  .on('error', function (err) {    
-    console.log("yo momma", err);                             
+  request.get(requestURL, options, cb)               
+  .on('error', function (err) {                              
     throw err;
   })      
 }
 
+function listAvatarUrls(error, response, body){
+  contributors = JSON.parse(body);
+  contributors.forEach(function(person) {
+    downloadImageByURL(person.avatar_url, "./avatars/" + person.login + ".jpg" );
+  });
+}
 
 function downloadImageByURL(url, filePath) {
-  request.get(url, options,function(error, response, body){
+  request.get(url, options,function(){
   })
   .pipe(fs.createWriteStream(filePath))
   .on('error', function (err) {
-    console.log(err);
     throw err;
   });
 }
 
 
-var options = {
-  headers: {
-    'User-Agent': "GitHub Avatar Downloader - Student Project"
+
+
+getRepoContributors(process.argv[2], process.argv[3], listAvatarUrls);
+
+/*
+  if (!fs.existsSync(filePath)){
+    fs.mkdirSync(filePath);
   }
-};
 
-getRepoContributors(process.argv[2], process.argv[3], function(err, result) {
-  console.log("Errors:", err);
-  console.log("Result:", result);
-});
 
-// downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "avatars/kvirani.jpg")
+*/
